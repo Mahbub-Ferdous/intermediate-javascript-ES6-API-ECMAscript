f3aa3fa57ff9bbc555405d90b271c63eb6cfb455
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2022-06-05 17:09:53 +0600

    dynamic data type description and concepts

---
+++ javascript-tricky-concepts/dynamic-data-type.js
@@ -0,0 +1,18 @@
+/// static data type =====>>  some language like c we declare a variable with data type keyword name in left side of equal(=) sign
+
+/// dynamic data type ====>> but in javascript we use let, const,sum variable keyword than variable name and the value in righht side of equal sign and javascript know type from the variable value and thhats why it is call dynamic type variable... example given below:
+
+/* let a = 10;
+console.log(typeof a); */         // ans is 'number' cause right side we pass a number 
+/* let a = "aksash rubel";
+console.log(typeof a);  */    // ans is string but same name just change the right side value  
+/* let a = null;
+console.log(typeof a)         /// objects  */
+/* let a = undefined;
+console.log(typeof a);  */       /// ans is underfined but ita a same variable name
+let a = false;
+console.log(typeof a);            /// ans is boolean but left side is same 
+
+
+
+///  so moral of the story is javascript dynamic type variable and know the data type depend on variable value 
